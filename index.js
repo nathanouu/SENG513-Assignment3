ca7aae5001a44a8ba06b15dac6b6065fa9dde692
hannah.wright <hannah.wright@zone41-ea.cs.ucalgary.ca>
2017-03-03 13:35:37 -0700

    Fixed whitespace.

--- index.js
+++ index.js
@@ -30,88 +30,88 @@ app.use(express.static(__dirname + '/public'));
 
 // listen to 'chat' messages
 io.on('connection', function(client){
-		client.on('new-user', function(){
-				let name = "Anonymous" + Math.floor((Math.random() * 100) + 1);
-				while(!unique_username(name)) // prevent duplicate usernames
-					name = "Anonymous" + Math.floor((Math.random() * 100) + 1);
-				online_users[client.id] = {username: name, colour: "default"};
-				user_joined();
-		});
+	client.on('new-user', function(){
+		let name = "Anonymous" + Math.floor((Math.random() * 100) + 1);
+		while(!unique_username(name)) // prevent duplicate usernames
+			name = "Anonymous" + Math.floor((Math.random() * 100) + 1);
+		online_users[client.id] = {username: name, colour: "default"};
+		user_joined();
+	});
 
-		client.on('returning-user', function(response) {
-				let old_name = response.username;
-				let old_colour = response.colour;
-				online_users[client.id] = {username: old_name, colour: old_colour};
-				user_joined();
-		});
+	client.on('returning-user', function(response) {
+		let old_name = response.username;
+		let old_colour = response.colour;
+		online_users[client.id] = {username: old_name, colour: old_colour};
+		user_joined();
+	});
 
-		function user_joined() {
-				client.emit('get-history', chat_history);
-				client.emit('update', online_users[client.id]);
-				client.emit('set-cookie', online_users[client.id]);
+	function user_joined() {
+		client.emit('get-history', chat_history);
+		client.emit('update', online_users[client.id]);
+		client.emit('set-cookie', online_users[client.id]);
 
-				io.emit('update-people', online_users);
-				io.emit('alert', online_users[client.id].username + " joined the conversation.");
-				console.log(online_users[client.id].username + " connected.");
-		}
+		io.emit('update-people', online_users);
+		io.emit('alert', online_users[client.id].username + " joined the conversation.");
+		console.log(online_users[client.id].username + " connected.");
+	}
 
-		client.on('chat', function(data){
-				let now = new  Date();
-				let colour_val = "default";
+	client.on('chat', function(data){
+		let now = new  Date();
+		let colour_val = "default";
 
-				// Regular expression source: https://obviate.io/2015/07/23/
-				let nick_test = /^\/nick ([a-zA-Z0-9]{0,20})/g;
-				let nick_request = nick_test.exec(data);
-				let colour_test = /^\/nickcolor ([0-9a-fA-F]{6})/g;
-				let colour_request = colour_test.exec(data);
+		// Regular expression source: https://obviate.io/2015/07/23/
+		let nick_test = /^\/nick ([a-zA-Z0-9]{0,20})/g;
+		let nick_request = nick_test.exec(data);
+		let colour_test = /^\/nickcolor ([0-9a-fA-F]{6})/g;
+		let colour_request = colour_test.exec(data);
 
-				if(nick_request != null) {
-						let new_username = nick_request[1];
-						let old_username = online_users[client.id].username;
-						if(!unique_username(new_username))
-								client.emit('alert', "The nickname " + new_username + " is already taken.");
-						else {
-								online_users[client.id] = {username: new_username, colour: "default"};
-								update_users();
-								io.emit('alert', old_username + " changed their nickname to " + new_username + ".");
-						}
-				} else if (colour_request != null) {
-						online_users[client.id].colour = colour_request[1];
-						update_users();
-						client.emit('alert', "Colors successfully changed.");
-				} else {
-						let message_data = { message: data,
-													 		   user: online_users[client.id],
-																 // Format date source: http://stackoverflow.com/questions/3552461/
-																 server_time: dateFormat(now, "mmmm dS, yyyy - h:MM TT")};
-				if(chat_history.length >= 200) // only store the last 200 messages
-						chat_history.splice(0,1);
-						chat_history.push(message_data);
-						io.emit('chat', message_data);
-		}});
-
-		client.on('disconnect', function() {
-				if(online_users[client.id] != null) {
-						let old_username = online_users[client.id].username;
-						io.emit('alert', old_username + " left the conversation.");
-						console.log(old_username + " disconnected");
+		if(nick_request != null) {
+				let new_username = nick_request[1];
+				let old_username = online_users[client.id].username;
+				if(!unique_username(new_username))
+					client.emit('alert', "The nickname " + new_username + " is already taken.");
+				else {
+					online_users[client.id] = {username: new_username, colour: "default"};
+					update_users();
+					io.emit('alert', old_username + " changed their nickname to " + new_username + ".");
 				}
-				delete online_users[client.id];
-				io.emit('update-people', online_users);
-		});
+		} else if (colour_request != null) {
+			online_users[client.id].colour = colour_request[1];
+			update_users();
+			client.emit('alert', "Colors successfully changed.");
+		} else {
+			let message_data = { message: data,
+										 		   user: online_users[client.id],
+													 // Format date source: http://stackoverflow.com/questions/3552461/
+													 server_time: dateFormat(now, "mmmm dS, yyyy - h:MM TT")};
+		if(chat_history.length >= 200) // only store the last 200 messages
+			chat_history.splice(0,1);
+			chat_history.push(message_data);
+			io.emit('chat', message_data);
+	}});
 
-		function update_users() {
-				client.emit('update', online_users[client.id]);
-				io.emit('update-people', online_users);
+	client.on('disconnect', function() {
+		if(online_users[client.id] != null) {
+			let old_username = online_users[client.id].username;
+			io.emit('alert', old_username + " left the conversation.");
+			console.log(old_username + " disconnected");
 		}
+		delete online_users[client.id];
+		io.emit('update-people', online_users);
+	});
 
-		function unique_username(name) {
-				let unique = true;
-				for(let property in online_users) {
-						if(!unique) break;
-						if(online_users[property] == name)
-							unique = false;
-				}
-				return unique;
+	function update_users() {
+		client.emit('update', online_users[client.id]);
+		io.emit('update-people', online_users);
+	}
+
+	function unique_username(name) {
+		let unique = true;
+		for(let property in online_users) {
+			if(!unique) break;
+			if(online_users[property] == name)
+				unique = false;
 		}
+		return unique;
+	}
 });
--- public/script/client.js
+++ public/script/client.js
@@ -12,129 +12,129 @@
 /***********************************************************************/
 
 $(function() {
-    var socket = io.connect('http://127.0.0.1:3000');
+  var socket = io.connect('http://127.0.0.1:3000');
 
-    let $input = $('#msg-box');
-    let $window = $(window);
-    let $messages = $('#messages');
-    let $current_username = $('#current-user');
-    let $current_online_users = $('#online-users');
+  let $input = $('#msg-box');
+  let $window = $(window);
+  let $messages = $('#messages');
+  let $current_username = $('#current-user');
+  let $current_online_users = $('#online-users');
 
-    $input.keypress(function(e) {
-       if (e.keyCode === 13) {
-          e.preventDefault();
-          socket.emit('chat', $input.val());
-          $input.val('');
-       }
-    });
-
-    socket.on('connect', function (data) {
-        var user = getCookie("username");
-        if (user != "") {
-            var user_colour = getCookie("colour");
-            socket.emit("returning-user", {username: user, colour: user_colour});
-        } else {
-            socket.emit("new-user");
-        }
-    });
-
-    socket.on('alert', function(response) {
-        let formatted = "<li class='message alert'>" + response + "</li>";
-        $messages.append(formatted);
-        scroll_down();
-    });
-
-    socket.on('set-cookie', function(response){
-        client_username = response.username;
-        client_colour = response.colour;
-        setCookie("username", client_username, 1);
-        setCookie("colour", client_colour, 1);
-    });
-
-    socket.on('get-history', function(response){
-        let history = response;
-        $.each(history, function(index, value) {
-            let message_html = display_message(value);
-            $messages.append(message_html);
-        });
-        scroll_down();
-    });
-
-    socket.on('update', function(response){
-        let client_username = response.username;
-        let client_colour = response.colour;
-        setCookie("username", client_username, 1);
-        setCookie("colour", client_colour, 1);
-
-        if(response.colour != "default")
-            $current_username.html("You are <font color='" + client_colour + "'>" + client_username + "</font>");
-        else
-            $current_username.html("You are " + client_username);
-    });
-
-    socket.on('update-people', function(response){
-        online_users = response;
-        $current_online_users.empty();
-        console.log(online_users);
-        $.each(online_users, function(index, value) {
-            let formatted = "<li><div class='fa fa-user'></div><div class='user'>";
-            if(value.colour != "default")
-                formatted += "<font color='" + value.colour + "'>" + value.username + "</font>"
-            else
-                formatted += value.username;
-
-            $current_online_users.append(formatted + "</div></li>");
-        });
-    });
-
-    socket.on('chat', function(response){
-        let message_html = display_message(response);
-        $messages.append(message_html);
-        scroll_down();
-    });
-
-
-    function display_message(message_data) {
-        let messsage_type;
-        if (message_data.user.username === getCookie("username"))
-            message_type = "self";
-        else
-            message_type = "other";
-
-        let formatted = "<li class='message " + message_type + "'><name>";
-        if(message_data.user.colour != "default")
-            formatted += "<font color='" + message_data.user.colour + "'>" + message_data.user.username + "</font>"
-        else
-            formatted += message_data.user.username;
-        formatted += "</name><time>" + message_data.server_time + "</time><p> " + message_data.message + "</p></li>";
-        return formatted;
+  $input.keypress(function(e) {
+    if (e.keyCode === 13) {
+      e.preventDefault();
+      socket.emit('chat', $input.val());
+      $input.val('');
     }
+  });
 
-    function scroll_down() {
-        let chat_div = document.getElementById("messages");
-        chat_div.scrollTop = chat_div.scrollHeight;
+  socket.on('connect', function (data) {
+    var user = getCookie("username");
+    if (user != "") {
+      var user_colour = getCookie("colour");
+      socket.emit("returning-user", {username: user, colour: user_colour});
+    } else {
+      socket.emit("new-user");
     }
+  });
 
-    /* Stolen from: https://www.w3schools.com/js/js_cookies.asp */
-    function setCookie(cname, cvalue, exdays) {
-        var d = new Date();
-        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-        var expires = "expires="+d.toUTCString();
-        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-    }
+  socket.on('alert', function(response) {
+    let formatted = "<li class='message alert'>" + response + "</li>";
+    $messages.append(formatted);
+    scroll_down();
+  });
 
-    function getCookie(cname) {
-        var name = cname + "=";
-        var ca = document.cookie.split(';');
-        for(var i = 0; i < ca.length; i++) {
-            var c = ca[i];
-            while (c.charAt(0) == ' ') {
-                c = c.substring(1);
-            }
-            if (c.indexOf(name) == 0) {
-                return c.substring(name.length, c.length);
-            }
-        }
-        return "";
+  socket.on('set-cookie', function(response){
+    client_username = response.username;
+    client_colour = response.colour;
+    setCookie("username", client_username, 1);
+    setCookie("colour", client_colour, 1);
+  });
+
+  socket.on('get-history', function(response){
+    let history = response;
+    $.each(history, function(index, value) {
+      let message_html = display_message(value);
+      $messages.append(message_html);
+    });
+    scroll_down();
+  });
+
+  socket.on('update', function(response){
+    let client_username = response.username;
+    let client_colour = response.colour;
+    setCookie("username", client_username, 1);
+    setCookie("colour", client_colour, 1);
+
+    if(response.colour != "default")
+      $current_username.html("You are <font color='" + client_colour + "'>" + client_username + "</font>");
+    else
+      $current_username.html("You are " + client_username);
+  });
+
+  socket.on('update-people', function(response){
+    online_users = response;
+    $current_online_users.empty();
+    console.log(online_users);
+    $.each(online_users, function(index, value) {
+      let formatted = "<li><div class='fa fa-user'></div><div class='user'>";
+      if(value.colour != "default")
+        formatted += "<font color='" + value.colour + "'>" + value.username + "</font>"
+      else
+        formatted += value.username;
+
+      $current_online_users.append(formatted + "</div></li>");
+    });
+  });
+
+  socket.on('chat', function(response){
+    let message_html = display_message(response);
+    $messages.append(message_html);
+    scroll_down();
+  });
+
+
+  function display_message(message_data) {
+    let messsage_type;
+    if (message_data.user.username === getCookie("username"))
+      message_type = "self";
+    else
+      message_type = "other";
+
+    let formatted = "<li class='message " + message_type + "'><name>";
+    if(message_data.user.colour != "default")
+      formatted += "<font color='" + message_data.user.colour + "'>" + message_data.user.username + "</font>"
+    else
+      formatted += message_data.user.username;
+    formatted += "</name><time>" + message_data.server_time + "</time><p> " + message_data.message + "</p></li>";
+    return formatted;
+  }
+
+  function scroll_down() {
+    let chat_div = document.getElementById("messages");
+    chat_div.scrollTop = chat_div.scrollHeight;
+  }
+
+  /* Stolen from: https://www.w3schools.com/js/js_cookies.asp */
+  function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+
+  function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ')
+        c = c.substring(1);
+
+      if (c.indexOf(name) == 0)
+        return c.substring(name.length, c.length);
+
     }
+    return "";
+  }
 });
